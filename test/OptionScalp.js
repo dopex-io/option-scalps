4cec3af772b39643b0c14d67db394cfe53a1a53f
tztokchad <tz@dopex.io>
2023-01-19 22:38:21 +0400

    add deploy test

--- test/OptionScalp.js
+++ test/OptionScalp.js
@@ -20,6 +20,7 @@ describe("Option Scalps", function () {
     "115792089237316195423570985008687907853269984665640564039457584007913129639935";
 
   const OPTION_PRICING = "0x2b99e3d67dad973c1b9747da742b7e26c8bdd67b";
+  const GMX_HELPER = "0xa028B56261Bb1A692C06D993c383c872B51AfB33", // GMX HELPER
 
   // 10th March 2022 8 AM UTC
   const EXPIRY = 1646899200;
@@ -42,5 +43,88 @@ describe("Option Scalps", function () {
     user0 = signers[1];
     user1 = signers[2];
     user2 = signers[3];
+  });
+
+  it("should deploy option scalp", async function () {
+    // USDC
+    const USDC = await ethers.getContractFactory("USDC");
+    usdc = await USDC.deploy();
+    // WETH
+    const WETH = await ethers.getContractFactory("WETH");
+    weth = await WETH.deploy();
+    // Price oracle
+    const PriceOracle = await ethers.getContractFactory("MockPriceOracle");
+    priceOracle = await PriceOracle.deploy();
+    // Volatility oracle
+    const VolatilityOracle = await ethers.getContractFactory(
+      "MockVolatilityOracle"
+    );
+    volatilityOracle = await VolatilityOracle.deploy();
+    // Option pricing
+    const OptionPricing = await ethers.getContractFactory("MockOptionPricing");
+    optionPricing = await OptionPricing.deploy();
+    // Uniswap factory
+    const UniswapFactory = await ethers.getContractFactory("UniswapV2Factory");
+    uniswapFactory = await UniswapFactory.deploy(owner.address);
+
+    // WETH-USDC pair
+    await uniswapFactory.createPair(weth.address, usdc.address);
+
+    // Uniswap v2 router
+    const UniswapV2Router = await ethers.getContractFactory(
+      "UniswapV2Router02"
+    );
+    uniswapV2Router = await UniswapV2Router.deploy(
+      uniswapFactory.address,
+      weth.address
+    );
+    // Uniswap v3 router
+    uniswapV3Router = await ethers.getContractAt(
+      "IUniswapV3Router",
+      "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+    );
+    // Gmx router
+    gmxRouter = await ethers.getContractAt(
+      "IGmxRouter",
+      "0xaBBc5F99639c9B6bCb58544ddf04EFA6802F4064"
+    );
+    
+    // Asset swapper
+    const AssetSwapper = await ethers.getContractFactory("AssetSwapper");
+
+    assetSwapper = await AssetSwapper.deploy(
+      uniswapV2Router.address,
+      uniswapV3Router.address,
+      gmxRouter.address,
+      weth.address
+    );
+    // Option Scalp
+    const OptionScalp = await ethers.getContractFactory(
+      "OptionScalp"
+    );
+    // address _base,
+    // address _quote,
+    // address _optionPricing,
+    // address _volatilityOracle,
+    // address _priceOracle,
+    // address _gmxRouter,
+    // address _gmxHelper,
+    // uint _minimumMargin
+    optionScalp = await OptionScalp.deploy(
+        weth.address,
+        usdc.address,
+        OPTION_PRICING,
+        volatilityOracle.address,
+        priceOracle.address,
+        gmxRouter.address,
+        GMX_HELPER,
+        "10000000", // $10
+    );
+
+    const wethBalance = await weth.balanceOf(owner.address);
+    const usdcBalance = await usdc.balanceOf(owner.address);
+
+    expect(wethBalance).to.equal("100000000000000000000000000000");
+    expect(usdcBalance).to.equal("10000000000000000000");
   });
 });
